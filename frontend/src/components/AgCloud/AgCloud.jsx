af31d2d956f84cf9a7bccda3d1218744143f3505
kumar rishabh <kumarrishabh@sjsu.edu>
2019-12-09 03:45:03 -0800

    new Nav bar changes

--- frontend/src/components/AgCloud/AgCloud.jsx
+++ frontend/src/components/AgCloud/AgCloud.jsx
@@ -16,8 +16,8 @@ import EdgeStation from './EdgeStation'
 import TotalAvailableEdgeStation from './TotalAvailableEdgeStation'
 import SensorData from './SensorData'
 import BillingComponent from './BillingComponent'
-
 import SMDashboardComponent from './SR/SMDashboardComponent.jsx'
+
 
 class AgCloud extends Component {
     render() {
@@ -39,8 +39,7 @@ class AgCloud extends Component {
                             <Route path="/sensordata" component={SensorData}/>
                             <Route path="/billing" component={BillingComponent}/>
                             <Route path="/servicerequest" component={SMDashboardComponent}/>
-                            <AuthenticatedRoute path="/logout" component={LogoutComponent}/>
-                                                      
+                            <AuthenticatedRoute path="/logout" component={LogoutComponent}/>                  
                             <Route component={ErrorComponent}/>
                         </Switch>
                         
